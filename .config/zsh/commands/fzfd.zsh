50e5b89874d680e3ffd5d1f389c9f44fd2352699
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-03-13 21:57:41 +0100

    fix(commands): zsh isn't case sensitive...

--- .config/zsh/commands/fzfd.zsh
+++ .config/zsh/commands/fzfd.zsh
@@ -1,6 +1,6 @@
 # Allows to select a child (or grandchild, etc) directory of the current directory using fzf
 # Usage: fzfd [path]
 fzfd() {
-  local path="${1:-$(pwd)}"
-  find "$path" -type d 2>/dev/null | fzf
+  local local_path="${1:-$(pwd)}"
+  find "$local_path" -type d 2>/dev/null | fzf
 }
