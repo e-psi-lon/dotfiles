d43bbceb0f7969e4791e6512453ba1301b0a047e
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-11-18 14:11:16 +0100

    feat(fzfd): Add the ability to pass options to fzf

--- .config/zsh/commands/fzfd.zsh
+++ .config/zsh/commands/fzfd.zsh
@@ -1,6 +1,7 @@
 # Allows to select a child (or grandchild, etc) directory of the current directory using fzf
-# Usage: fzfd [path]
+# Usage: fzfd [path] <fzf options>
 fzfd() {
   local local_path="${1:-$(pwd)}"
-  find "$local_path" -type d 2>/dev/null | fzf
+  shift
+  find "$local_path" -type d 2>/dev/null | fzf $@
 }
