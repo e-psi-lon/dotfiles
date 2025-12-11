2bcf496599de061b21f96f4e401a30df6ad911b2
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-12-11 14:05:44 +0100

    chore: Update to NixOS 25.11

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -3,7 +3,7 @@
     
     inputs = {
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-	nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+	nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
 	zen-browser.url = "github:youwen5/zen-browser-flake";
     };
 
