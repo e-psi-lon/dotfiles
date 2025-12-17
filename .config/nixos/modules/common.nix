8a07e2838e2df488e01d96eb0b513a551230d990
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-12-17 21:06:59 +0100

    feat(spotify): Add Spotify and Spicetify for music

--- .config/nixos/modules/common.nix
+++ .config/nixos/modules/common.nix
@@ -106,7 +106,7 @@
 
     users.users.e-psi-lon = {
         isNormalUser = true;
-	    description = "e_ψ_lon";
+        description = "e_ψ_lon";
         extraGroups = [ "wheel" "networkmanager" "audio" "video" "input" "storage" ];
         shell = pkgs.zsh;
         hashedPassword = "$6$RuWluSDrWlxlvRqQ$JCiJFbeooSAfaKV0BkcCx6g/wxxSH9oDpUbAn5EG6Ee/aG5hZ3zt9UVKUnRxbF6ELDFj71yvUdEpi/.aglaj1/";
