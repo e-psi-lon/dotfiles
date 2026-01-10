648535d5c803a4d8e15cbea164fa1bfc6627781e
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-01-10 14:00:04 +0100

    feat(nixos-asus): Add sunshine service to stream the device's screen

--- .config/nixos/hosts/nixos-asus.nix
+++ .config/nixos/hosts/nixos-asus.nix
@@ -64,18 +64,26 @@
         "d /mnt/data 0775 root data - -"
     ];
 
-    services.openvpn.servers = {
-        univ-tours = { 
-            config = '' config /home/e-psi-lon/.config/openvpn/vpnfr-etu-TCP4-443.ovpn '';
-            up = ''
-                ${pkgs.systemd}/bin/resolvectl dns tun0 10.195.2.1 10.196.20.10 10.195.2.2
-                ${pkgs.systemd}/bin/resolvectl domain tun0 '~univ-tours.local' '~univ-tours.fr'
-            '';
-                
-            down = ''
-                ${pkgs.systemd}/bin/resolvectl revert tun0
-            '';
-            autoStart = false;
+    services = {
+        sunshine = {
+            enable = true;
+            autoStart = true;
+            capSysAdmin = true;
+            openFirewall = true;
+        };
+        openvpn.servers = {
+            univ-tours = { 
+                config = '' config /home/e-psi-lon/.config/openvpn/vpnfr-etu-TCP4-443.ovpn '';
+                up = ''
+                    ${pkgs.systemd}/bin/resolvectl dns tun0 10.195.2.1 10.196.20.10 10.195.2.2
+                    ${pkgs.systemd}/bin/resolvectl domain tun0 '~univ-tours.local' '~univ-tours.fr'
+                '';
+                    
+                down = ''
+                    ${pkgs.systemd}/bin/resolvectl revert tun0
+                '';
+                autoStart = false;
+            };
         };
     };
 
