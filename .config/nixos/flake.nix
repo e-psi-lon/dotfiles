6c9f753ed2a10587306e9a0cd253a8aaa43e75d0
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-01-11 23:05:45 +0100

    feat(nixcord): Start working on a Discord configuration with Vencord and Nixcord

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -5,11 +5,12 @@
         nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
         nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
         zen-browser.url = "github:youwen5/zen-browser-flake";
+        spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+        nixcord.url = "github:kaylorben/nixcord";
         minegrub-world-sel-theme = {
             url = "github:Lxtharia/minegrub-world-sel-theme";
             inputs.nixpkgs.follows = "nixpkgs-stable";
         };
-        spicetify-nix.url = "github:Gerg-L/spicetify-nix";
         home-manager-unstable = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs-unstable";
@@ -20,7 +21,7 @@
         };
     };
 
-    outputs = { self, nixpkgs-unstable, nixpkgs-stable, zen-browser, minegrub-world-sel-theme, spicetify-nix, home-manager-unstable, home-manager-stable, ... }:
+    outputs = { self, nixpkgs-unstable, nixpkgs-stable, zen-browser, spicetify-nix, nixcord, minegrub-world-sel-theme, home-manager-unstable, home-manager-stable, ... }:
     let
         mkNixosSystem = { pkgs, home-manager, modules, machineName, extraArgs ? {} }:
             pkgs.lib.nixosSystem {
