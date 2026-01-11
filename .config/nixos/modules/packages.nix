598c2da69e352e2a46b52a07e8aa16edb502e868
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-01-11 13:15:36 +0100

    chore(packages): Clean up system packages by removing homoe-managed entries

--- .config/nixos/modules/packages.nix
+++ .config/nixos/modules/packages.nix
@@ -2,45 +2,14 @@
 
 {
   environment.systemPackages = (with pkgs; [
-    # CLI Tools
-    wget
+    # System utilities
+    pciutils
+    usbutils
+    
+    # Basic utilities
+    stow # TODO: Remove once dotfiles are migrated to home-manager
     git
-    fzf
-    fastfetch
-    atuin
-    gh
-    eza
-    stow
-    nano
     rsync
     curl
-    zip
-    unzip
-    htop
-    tree
-    zoxide
-    oh-my-posh
-
-    # Development
-    neovim
-    uv
-    python3
-    php
-    gcc
-    nasm
-
-    # Media
-    ffmpeg
-
-    # GUI Applications
-    firefox
-    libreoffice-qt
-    prismlauncher
-
-    # Network
-    networkmanager
-    waypipe
-    openvpn
-    vscode
   ]);
 }
