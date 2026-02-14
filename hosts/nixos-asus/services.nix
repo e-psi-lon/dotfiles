14050f79dd38eea89487ee5f9c9db395d668fcd8
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-02-14 20:09:47 +0100

    refactor(nixos-asus): Use the pre-packaged Numworks rule instead of a manual one

--- hosts/nixos-asus/services.nix
+++ hosts/nixos-asus/services.nix
@@ -1,4 +1,4 @@
-{ pkgs, hashes, ... }:
+{ pkgs, ... }:
 {
 
   services = {
@@ -25,17 +25,8 @@
         autoStart = false;
       };
     };
-    udev.packages = [
-      (pkgs.writeTextFile {
-        name = "numworks-udev-rule";
-        destination = "/etc/udev/rules.d/50-numworks-calculator.rules";
-        text = builtins.readFile (
-          pkgs.fetchurl {
-            url = "https://cdn.numworks.com/f2be8a48/50-numworks-calculator.rules";
-            sha256 = hashes.udev.numworks-rules;
-          }
-        );
-      })
+    udev.packages = with pkgs; [
+      numworks-udev-rules
     ];
   };
 }
