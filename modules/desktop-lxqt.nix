8a0a76aa99f5f3b91ecb4089676b2addcba98496
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-05-18 16:14:38 +0200

    refactor(desktop-lxqt): Add all missing settings for a proper session and drop redundant package inclusion

--- modules/desktop-lxqt.nix
+++ modules/desktop-lxqt.nix
@@ -1,22 +1,29 @@
-{ pkgs, ... }:
-
+{ pkgs, lib, ... }:
 {
   services = {
     xserver = {
-      enable = true;
-      desktopManager.lxqt = {
-        enable = true;
-        extraPackages = with pkgs; [
-          lxqt.qterminal
-          lxqt.pcmanfm-qt
-          lxqt.lxqt-runner
-          lxqt.lxqt-wayland-session
-          labwc
-          openbox
-        ];
-      };
+       enable = true;
+       desktopManager.lxqt.enable = true;
     };
+    displayManager = {
+      sddm.enable = true;
+      sessionPackages = [ pkgs.lxqt.lxqt-wayland-session ];
+    };
+    graphical-desktop.enable = true;
+  };
 
-    displayManager.sddm.enable = true;
+  environment.systemPackages = [ pkgs.labwc ];
+  security = {
+    polkit.enable = true;
+    pam.services.swaylock = {};
+  };
+  programs = {
+    dconf.enable = true;
+    xwayland.enable = true;
+  };
+  xdg.portal = {
+    wlr.enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.wlroots.default = lib.mkDefault [ "wlr" "gtk" ];
   };
 }
