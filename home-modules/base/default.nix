e9a5d3c9ce3a33c95d487e88b9ecfdf9d53cc58d
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-06-26 20:48:23 +0200

    chore(home-modules): Move the EDITOR environment variable to the proper nivxim reference

--- home-modules/base/default.nix
+++ home-modules/base/default.nix
@@ -56,7 +56,7 @@
         # It's important to be kind with your computer :)
         please = "sudo";
       };
-      sessionVariables.EDITOR = lib.getExe pkgs.neovim;
+      sessionVariables.EDITOR = config.programs.nixvim.package;
     };
     xdg.enable = true;
     programs.nixvim.imports = [ ./neovim.nix ];
