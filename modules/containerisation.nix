6ef940492120d43c66fd507df30c4f48b8be0a92
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-08-04 16:13:38 +0200

    fix(networking): Correct local traffic handling and handle IPv6

--- modules/containerisation.nix
+++ modules/containerisation.nix
@@ -24,27 +24,38 @@
       }
     ];
   };
+  networking = {
+    firewall.allowedTCPPorts = [ 
+      50080 
+      50443
+    ];
+    nftables = {
+      enable = true;
+      tables = let 
+        content = ''
+          chain prerouting {
+            type nat hook prerouting priority -100;
+            tcp dport 80 redirect to :50080
+            tcp dport 443 redirect to :50443
+          }
 
-  networking.nftables.enable = true;
-  networking.nftables.tables.port-forward = {
-    family = "ip";
-    content = ''
-      chain prerouting {
-        type nat hook prerouting priority -100;
-        tcp dport 80 redirect to :50080
-        tcp dport 443 redirect to :50443
-      }
-
-      chain output {
-        type nat hook output priority -100;
-        ip daddr 127.0.0.1 tcp dport 80 redirect to :50080
-        ip daddr 127.0.0.1 tcp dport 443 redirect to :50443
-      }
-    '';
+          chain output {
+            type nat hook output priority -100;
+            fib daddr type local tcp dport 80 redirect to :50080
+            fib daddr type local tcp dport 443 redirect to :50443
+          }
+        '';
+      in {
+        port-forward-v6 = {
+          family = "ip6";
+          content = content;
+        };
+        port-forward-v4 = {
+          family = "ip";
+          content = content;
+        };
+      };
+    };
   };
-  networking.firewall.allowedTCPPorts = [
-    50080
-    50443
-  ];
 
 }
